6184febbce8111ff5f9ab0d4d59ee13390d3dc64
Jonas Leite <jonas.leite@zumpy.com.br>
2025-05-03 00:06:29 -0300

    feat: Create the file for estilization

---
+++ style.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+
+
+ const styles = StyleSheet.create({
+    container:{
+        flex:1, 
+        justifyContent:'center', 
+        alignItems:'center'
+    },
+    text:{
+        fontSize:32,
+        fontWeight:700,
+        color:'#0004FF'
+    }
+})
+
+export default styles
