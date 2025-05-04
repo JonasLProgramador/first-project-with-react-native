474dd2048f99a0f90098fdc30b40fba66ba29a8f
Jonas Leite <jonas.leite@zumpy.com.br>
2025-05-03 22:11:01 -0300

    feat: create component input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,7 @@
+import { TextInput,TextInputProps } from "react-native"
+import styles from './style'
+const Input = ({...props}:TextInputProps) =>{
+    return <TextInput style={styles.container}  {...props}/>
+}
+
+export default Input
